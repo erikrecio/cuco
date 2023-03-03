49ecb3649eaec05d964b76e82e7fdda1f7e7fa85
Erik <erikrecio@gmail.com>
2023-03-03 11:04:53 +0100

    bs

--- Prueba reuploading/Probes.py
+++ Prueba reuploading/Probes.py
@@ -9,7 +9,7 @@ num_test = 2000
 Xdata, y_train = circle(num_training)
 X_train = np.hstack((Xdata, np.zeros((Xdata.shape[0], 1))))
 
-num_layers = 3
+num_layers = 1
 params = np.random.uniform(size=(num_layers + 1, 15))
 M = density_matrix(state_labels[0])
 
@@ -32,16 +32,14 @@ def circuit(params, x, y):
     for i,p in enumerate(params):
         
         if i != 0:
-            qml.Rot(x[0,0], 0, 0, wires=0)
-            qml.Rot(x[0,1], 0, 0, wires=1)
+            qml.RY(x[0,0], wires=0)
+            qml.RY(x[0,1], wires=1)
         U_SU4(p, [0,1])
     
     return qml.expval(qml.Hermitian(y, wires=[0]))
 
 
-print(X_train)
 resultat = circuit(params, X_train, M)
-print(resultat)
 
 dev._circuit.draw(output ="mpl", interactive = True)
 # %%
